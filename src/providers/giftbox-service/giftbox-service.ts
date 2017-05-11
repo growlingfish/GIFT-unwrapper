0a69f58414f0f4725c2d0b6cb1529960f72ab694
Ben Bedwell <github@growlingfish.com>
2017-05-11 20:49:47 +0100

    API

--- src/providers/giftbox-service/giftbox-service.ts
+++ src/providers/giftbox-service/giftbox-service.ts
@@ -4,8 +4,18 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { GlobalVarProvider } from '../../providers/global-var/global-var';
 
+export class Gift {
+  id: number;
+
+  constructor(id: number) {
+    this.id = id;
+  }
+}
+
 @Injectable()
 export class GiftboxServiceProvider {
+
+  gifts: Array<Gift>;
 
   constructor(private globalVar: GlobalVarProvider, public http: Http) {}
 
@@ -14,7 +24,8 @@ export class GiftboxServiceProvider {
       this.http.get(this.globalVar.getGiftsURL(email))
         .map(response => response.json())
         .subscribe(data => {
-          console.log(data);
+          this.gifts = data.gifts;
+          console.log(this.gifts);
           observer.next(true);
           observer.complete();
         },
